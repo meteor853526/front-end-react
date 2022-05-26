1dad1f9a1840ffc9778199e145377d06b0533827
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2022-05-26 22:03:15 +0800

    Update AddProduct.js

--- src/product/AddProduct.js
+++ src/product/AddProduct.js
@@ -27,7 +27,9 @@ class AddProduct extends Component {
     }
    
     onImageChange = e => {
-        this.setState({[e.target.name]:e.target.files})
+
+        this.setState({[e.target.name]:e.target.files[0]})
+        console.log(e.target.files[0])
         // if (event.target.files && event.target.files[0]) {
         //   let img = event.target.files[0];
         //   this.setState({
@@ -42,28 +44,29 @@ class AddProduct extends Component {
         const formData = new FormData();
   
         formData.append("file", this.state.file);
-        
-        
-        console.log(...formData)
+        formData.append("name", this.state.name);
+        formData.append("category", this.state.category);
+        formData.append("price", this.state.price);
+        formData.append("type", this.state.type);
+        formData.append("number", this.state.number);
+        formData.append("introduce", this.state.introduce);
+        formData.append("owner", this.props.user.user2);
+        //console.log(...formData)
         
         axios({
             url: 'http://localhost:8080/api/v1/product/addP',
             method: "Post",
             data: formData,
-            headers: {
-                "Content-type": "multipart/form-data",
-                
-            },
-            params:{
-                'name':this.state.name,
-                'category': this.state.category,
-                'price':this.state.price,
-                'type': this.state.type,
-                'number':this.state.number,
-                'introduce':this.state.introduce,
-                'owner':this.props.user.user2,
-                'file':this.state.file
-            }
+            // params:{
+            //     // 'name':this.state.name,
+            //     'category': this.state.category,
+            //     'price':this.state.price,
+            //     'type': this.state.type,
+            //     'number':this.state.number,
+            //     'introduce':this.state.introduce,
+            //     'owner':this.props.user.user2,
+            //     'file':this.state.file
+            // }
         })
         .then(function (response) {
             console.log(response);
@@ -95,8 +98,9 @@ class AddProduct extends Component {
     
                         <textarea type="text"class="form-control mb-4" id="introduce" name="introduce"value={introduce}placeholder="Product Description"onChange={this.changeHandler}>Product Description</textarea>
                         <div class="custom-file mb-4">
-                            <input type="file" name="file"accept="image/png,image/gif,image/jpeg" class="custom-file-input"id="image"onChange={this.onImageChange}/>
+                            <input type="file" name="file"accept="image/png,image/gif,image/jpeg" class="custom-file-input"id="file"onChange={this.onImageChange} multiple/>
                             <label class="custom-file-label"for="customFile">Product Image</label>
+                            
                         </div>
                         
                         <button class="btn btn-info btn-block" type="submit">add</button>
