32241843e777dc0f3bdb739225e06a2ec3730e49
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2022-06-02 19:21:38 +0800

    更

--- src/checkout/Check_type.js
+++ src/checkout/Check_type.js
@@ -26,7 +26,7 @@ const Check_type =() =>{
     }
 
     const submitHandler = () => {
-        
+        alert('????????')
     }
    
     
@@ -39,114 +39,117 @@ const Check_type =() =>{
         <div class="all" style={{"width":"500px"}}>
             <h2>選擇結帳方式</h2>
             
-            <div className="check_type">
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio"value="信用卡一次付清" name="pay_type" id="flexRadioDefault1" onChange={() => setpay_type("信用卡一次付清")}/>
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            信用卡一次付清
-                        </label>
-                        </div>
-                    </li>
-                    
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio" value="AFTEE先享後付"name="pay_type" id="flexRadioDefault2"onChange={() => setpay_type("AFTEE先享後付")}/>
-                        <label class="form-check-label" for="flexRadioDefault2">
-                        AFTEE先享後付
-                        </label>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio"value="悠遊付" name="pay_type" id="flexRadioDefault3"onChange={() => setpay_type("悠遊付")}/>
-                        <label class="form-check-label" for="flexRadioDefault3">
-                        悠遊付
-                        </label>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio" value="超商取貨付款"name="pay_type" id="flexRadioDefault4"onChange={() => setpay_type("超商取貨付款")}/>
-                        <label class="form-check-label" for="flexRadioDefault4">
-                        超商取貨付款
-                        </label>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio" value="ATM付款"name="pay_type" id="flexRadioDefault5"onChange={() => setpay_type("ATM付款")}/>
-                        <label class="form-check-label" for="flexRadioDefault5">
-                        ATM付款
-                        </label>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio" value="街口支付"name="pay_type" id="flexRadioDefault6"onChange={() => setpay_type("街口支付")}/>
-                        <label class="form-check-label" for="flexRadioDefault6">
-                        街口支付
-                        </label>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="form-check">
-                        <input class="form-check-input" type="radio"value="LINE Pay" name="pay_type" id="flexRadioDefault7"onChange={() => setpay_type("LINE Pay")}/>
-                        <label class="form-check-label" for="flexRadioDefault7">
-                            LINE Pay
-                        </label>
-                        </div>
-                    </li>
-                    
-                </ul>
-                    
-                
-            </div>
-
-            <div className="check_type" style={{marginTop:'100px'}}>
-                <h2 style={{marginleft:"100px"}}>選擇運送方式</h2>
-                <ul class="list-group">
+            <form>
+                <div className="check_type">
+                    <ul class="list-group">
                         <li class="list-group-item">
                             <div class="form-check">
-                            <input class="form-check-input" type="radio"value="宅配" name="delivery_type" id="flexRadioDefault11"onChange={() => setdelivery_type("宅配")}/>
-                            <label class="form-check-label" for="flexRadioDefault8">
-                                宅配
-                            </label>
-                            </div>
-                        </li>
-                        <li class="list-group-item">
-                            <div class="form-check">
-                            <input class="form-check-input" type="radio"value="7-ELEVEN取貨" name="delivery_type" id="flexRadioDefault8"onChange={() => setdelivery_type("7-ELEVEN取貨")}/>
-                            <label class="form-check-label" for="flexRadioDefault8">
-                                7-ELEVEN取貨
+                            <input class="form-check-input" type="radio"value="信用卡一次付清" name="pay_type" id="flexRadioDefault1"  onChange={() => setpay_type("信用卡一次付清")} />
+                            <label class="form-check-label" for="flexRadioDefault1">
+                                信用卡一次付清
                             </label>
                             </div>
                         </li>
                         
                         <li class="list-group-item">
                             <div class="form-check">
-                            <input class="form-check-input" type="radio"value="萊爾富取貨" name="delivery_type" id="flexRadioDefault9"onChange={() => setdelivery_type("萊爾富取貨")}/>
-                            <label class="form-check-label" for="flexRadioDefault9">
-                                萊爾富取貨
+                            <input class="form-check-input" type="radio" value="AFTEE先享後付"name="pay_type" id="flexRadioDefault2"onChange={() => setpay_type("AFTEE先享後付")} />
+                            <label class="form-check-label" for="flexRadioDefault2">
+                            AFTEE先享後付
                             </label>
                             </div>
                         </li>
                         <li class="list-group-item">
                             <div class="form-check">
-                            <input class="form-check-input" type="radio"value="全家取貨" name="delivery_type" id="flexRadioDefault10" onChange={() => setdelivery_type("全家取貨")}/>
-                            <label class="form-check-label" for="flexRadioDefault10">
-                                全家取貨
+                            <input class="form-check-input" type="radio"value="悠遊付" name="pay_type" id="flexRadioDefault3"onChange={() => setpay_type("悠遊付")}/>
+                            <label class="form-check-label" for="flexRadioDefault3">
+                            悠遊付
                             </label>
                             </div>
-                        </li>  
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" value="超商取貨付款"name="pay_type" id="flexRadioDefault4"onChange={() => setpay_type("超商取貨付款")} />
+                            <label class="form-check-label" for="flexRadioDefault4">
+                            超商取貨付款
+                            </label>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" value="ATM付款"name="pay_type" id="flexRadioDefault5"onChange={() => setpay_type("ATM付款")} />
+                            <label class="form-check-label" for="flexRadioDefault5">
+                            ATM付款
+                            </label>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" value="街口支付"name="pay_type" id="flexRadioDefault6"onChange={() => setpay_type("街口支付")} />
+                            <label class="form-check-label" for="flexRadioDefault6">
+                            街口支付
+                            </label>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio"value="LINE Pay" name="pay_type" id="flexRadioDefault7"onChange={() => setpay_type("LINE Pay")} />
+                            <label class="form-check-label" for="flexRadioDefault7">
+                                LINE Pay
+                            </label>
+                            </div>
+                        </li>
+                        
                     </ul>
-                   
+                    
+                    
+                </div>
+            
+                <div className="check_type" style={{marginTop:'100px'}}>
+                    <h2 style={{marginleft:"100px"}}>選擇運送方式</h2>
+                    <ul class="list-group">
+                            <li class="list-group-item">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio"value="宅配" name="delivery_type" id="flexRadioDefault11"onChange={() => setdelivery_type("宅配")} />
+                                <label class="form-check-label" for="flexRadioDefault8">
+                                    宅配
+                                </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio"value="7-ELEVEN取貨" name="delivery_type" id="flexRadioDefault8"onChange={() => setdelivery_type("7-ELEVEN取貨")} />
+                                <label class="form-check-label" for="flexRadioDefault8">
+                                    7-ELEVEN取貨
+                                </label>
+                                </div>
+                            </li>
+                            
+                            <li class="list-group-item">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio"value="萊爾富取貨" name="delivery_type" id="flexRadioDefault9"onChange={() => setdelivery_type("萊爾富取貨")}/>
+                                <label class="form-check-label" for="flexRadioDefault9">
+                                    萊爾富取貨
+                                </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio"value="全家取貨" name="delivery_type" id="flexRadioDefault10" onChange={() => setdelivery_type("全家取貨")}/>
+                                <label class="form-check-label" for="flexRadioDefault10">
+                                    全家取貨
+                                </label>
+                                </div>
+                            </li>  
+                        </ul>
+                        <h5>{pay_type}</h5>
 
-                    <Link to='/Data_page'
-                          state={{ pay_type : pay_type, delivery_type:delivery_type }} className="cus_btn"><button className="cus_btn"  onClick={() => submitHandler()}>填寫配送資料</button></Link>
-            </div>
+                        <Link to='/Data_page'
+                            state={{ pay_type : pay_type, delivery_type:delivery_type }} className="cus_btn"><button type="submit" className="cus_btn">填寫配送資料</button></Link>
+                </div>
+            </form>
         </div>
+        
         </div>
         );
     
--- src/checkout/Data_page.js
+++ src/checkout/Data_page.js
@@ -1,4 +1,4 @@
-import { Link,useLocation } from "react-router-dom";
+import { Link,useLocation,useNavigate } from "react-router-dom";
 import React,{Component} from "react";
 import axios from 'axios';
 import FormData from 'form-data'
@@ -8,6 +8,7 @@ import testservice from '../services/testservice';
 
 const DataPage = (props) => {
     const location = useLocation()
+    let history=useNavigate();
     const { pay_type, delivery_type} = location.state
     const [Fname, setFname] = useState()
     const [Lname, setLname] = useState()
@@ -15,9 +16,8 @@ const DataPage = (props) => {
     const [address, setaddress] = useState()
     const [time, settime] = useState()
     const [productid, setproductid] = useState()
-
-    console.log(pay_type)
-    console.log(delivery_type)
+    var count = 0
+    
 
     const [id, setid] = useState([])
 
@@ -64,10 +64,17 @@ const DataPage = (props) => {
   
         //var data = this.props.location.state;
     
-   
+    
+    const check =()=> {
+       
+        if(Fname === '' || Lname === '' || phone === '' || address === ''){
+            alert('表單請填完整')
+        }
+    }
     
     
     const submitHandler =(e) =>{
+        
         e.preventDefault();
         axios({
             url: 'http://localhost:8080/api/v1/Order/take',
@@ -88,34 +95,42 @@ const DataPage = (props) => {
         })
         .then(function (response) {
             console.log(response);
+            alert("訂單成功建立")
+            history('/BuyerOrder')
         })
         .catch(function (error) {
             console.log(error);
         });
         
     }
-
+    // if(Fname === '' || Lname === '' || phone === '' || address === ''){
+    //     document.getElementById('switch').style.display = 'none';
+    // }else{
+    //     document.getElementById('switch').style.display = 'flex';
+    // }
     
        
         
         return ( 
-            <div class="">
+            <div class="data_page" style={{width:"40vw"}}>
                 
                 {/* <h1>{this.props.pay_type}</h1> */}
+                <label for="basic-url" class="form-label"><h2>名字</h2></label>
                 <div class="input-group mb-3">
+                    
                 <span class="input-group-text" id="basic-addon1">姓氏</span>
-                <input type="text" class="form-control" placeholder="Username" value={Fname}aria-label="Username" aria-describedby="basic-addon1"onChange={e => setFname(e.target.value)}/>
+                <input type="text" class="form-control" placeholder="姓氏" value={Fname}aria-label="Username" aria-describedby="basic-addon1"onChange={e => setFname(e.target.value)}/>
                 
                 </div>
                 {/* <h1>{moment().format('YYYY-MM-DD HH:mm:ss')}</h1> */}
                 <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">名字</span>
-                <input type="text" class="form-control" placeholder="Recipient's username"value={Lname} aria-label="Recipient's username" aria-describedby="basic-addon2"onChange={e => setLname(e.target.value)}/>
+                <input type="text" class="form-control" placeholder="名字"value={Lname} aria-label="Recipient's username" aria-describedby="basic-addon2"onChange={e => setLname(e.target.value)}/>
                 {/* <span class="input-group-text" id="basic-addon2"></span> */}
               
                 </div>
 
-                <label for="basic-url" class="form-label">聯絡資訊及地址</label>
+                <label for="basic-url" class="form-label"><h2>聯絡資訊及地址</h2></label>
                 <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon3">手機號碼</span>
                 <input type="text" class="form-control" id="basic-url" value={phone}aria-describedby="basic-addon3"onChange={e => setphone(e.target.value)}/>
@@ -138,39 +153,66 @@ const DataPage = (props) => {
                 <textarea class="form-control" aria-label="With textarea"></textarea>
                 </div> */}
                 {/* <!-- Button trigger modal --> */}
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                Launch static backdrop modal
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={check}>
+                點擊送出
                 </button>
 
                 {/* <!-- Modal --> */}
-                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <h5>{Fname}</h5> <h5>{Lname}</h5>
-                        <h5>{address}</h5>
-                        <h5>{phone}</h5>   
-                        ...
-                        {
-                        id.map(
-                    
-                            id => 
-                                <h1>{id.productid} {id.productName} {id.number} {id.price}</h1>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div id = "switch" class="modal-dialog modal-dialog-scrollable">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="staticBackdropLabel">訂單</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <p class="mb-1 text-end" style ={{display : 'none'}}>價格 : {count = 0}</p>
+                        <div class="modal-body">
+                            <h2>名字: {Fname}{Lname}</h2>
+                            <h2>地址: {address}</h2>
+                            <h2>電話: {phone}</h2> 
+                            <h2>結帳方式: {pay_type}</h2>
+                            <h2>運送方式: {delivery_type}</h2>  
+                            
+                            {
+                            id.map(
+                        
+                                id => 
+                                <a class="list-group-item list-group-item-action" alt ="" disabled style ={{'height': "20vh"}}>
+                                <div class="d-flex w-100 justify-content-between">
+                                   
+                                    
+                                
+                                
+                                </div>
+                                <p class="mb-1 text-end" style ={{display : 'none'}}>價格 : {count += (id.price * id.number)}</p>
+                                <h2 class="mb-1" style={{color : 'blue'}}>商品名稱: {id.productName}</h2>
+                                <p class="mb-1 text-end"style={{ 'padding' : "1px", color:"red"}} >價格 : {id.price * id.number}</p>
+                                <div class="flex_cut">
+                                           
+                                    <h2 > 數量 : {id.number}</h2>
+                                       
+                                </div>
+                                
+                                    
+                            
+                                </a>
 
-                                )
+                                    )
 
-                            }
+                                }
+                            <div className="all_price" >
+                                <div className=""  style ={{ marginTop:'10px',color:'red'}}>
+                                    <h2>總價格: {count}</h2>
+                                
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={submitHandler}>送出</button>
+                        </div>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={submitHandler}>Understood</button>
-                    </div>
-                    </div>
-                </div>
                 </div>
                 
             </div>
